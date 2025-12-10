2e6b5ccf44730f8fb296bdc0a7e1a0d65e462e35
GuiiCordeiro <g.cordeirodossantos@outlook.com>
2025-12-10 18:49:04 -0300

    aula 11 ex008

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade é ${vel}`)
+if(vel > 60.5){
+   console.log(`Você ultrapassou o limite de velocidade. Multado`)
+}
+console.log(`Utilize sempre cinto de segurança`)
